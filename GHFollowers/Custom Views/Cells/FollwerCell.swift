f47cb117848e0ab7847cedc72c2e16a6db408c98
BisheshDangol <bishesh02dangol@gmail.com>
2025-07-22 18:57:10 +0545

    fix GFAvatarImageView: Setup complete

---
+++ GHFollowers/Custom Views/Cells/FollwerCell.swift
@@ -0,0 +1,12 @@
+//
+//  FollwerCell.swift
+//  GHFollowers
+//
+//  Created by Bishesh Dangol on 22/07/2025.
+//
+
+import UIKit
+
+class FollwerCell: UICollectionViewCell {
+    
+}
---
+++ GHFollowers/Custom Views/GFAvatarImageView.swift
@@ -0,0 +1,20 @@
+//
+//  GFAvatarImageView.swift
+//  GHFollowers
+//
+//  Created by Bishesh Dangol on 22/07/2025.
+//
+
+import UIKit
+
+class GFAvatarImageView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
